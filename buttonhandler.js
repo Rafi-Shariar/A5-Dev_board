78120dbefcc942af68858a8c24c723e05afd0f52
Rafi <rafi.shariar619@gmail.com>
2025-03-01 14:45:12 +0600

    added Q&A page

---
+++ buttonhandler.js
@@ -0,0 +1,60 @@
+const card1 = document.getElementById('Card1');
+card1.addEventListener('click',function(event){
+    event.preventDefault();
+    AlertFuction();
+    UpdateTaskNo();
+    UpdateActivityLog('Card1');
+    DisableButton("card1btn");
+
+});
+
+const card2 = document.getElementById('Card2');
+card2.addEventListener('click',function(event){
+    event.preventDefault();
+    AlertFuction();
+    UpdateTaskNo();
+    UpdateActivityLog('Card2');
+    DisableButton("card2btn");
+
+});
+
+const card3 = document.getElementById('Card3');
+card3.addEventListener('click',function(event){
+    event.preventDefault();
+    AlertFuction();
+    UpdateTaskNo();
+    UpdateActivityLog('Card3');
+    DisableButton("card3btn");
+
+});
+
+
+const card4 = document.getElementById('Card4');
+card4.addEventListener('click',function(event){
+    event.preventDefault();
+    AlertFuction();
+    UpdateTaskNo();
+    UpdateActivityLog('Card4');
+    DisableButton("card4btn");
+
+});
+
+const card5 = document.getElementById('Card5');
+card5.addEventListener('click',function(event){
+    event.preventDefault();
+    AlertFuction();
+    UpdateTaskNo();
+    UpdateActivityLog('Card5');
+    DisableButton("card5btn");
+
+});
+
+const card6 = document.getElementById('Card6');
+card6.addEventListener('click',function(event){
+    event.preventDefault();
+    AlertFuction();
+    UpdateTaskNo();
+    UpdateActivityLog('Card6');
+    DisableButton("card6btn");
+
+});
